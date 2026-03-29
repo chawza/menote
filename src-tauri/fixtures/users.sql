bce88363bdc150eccfaa1cb95d4f4ebb46900ceb
Nabeel KM <nabeelkahlil403@gmail.com>
2026-03-29 17:14:20 +0700

    users table migrations and create fixtures

---
+++ src-tauri/fixtures/users.sql
@@ -0,0 +1,12 @@
+PRAGMA foreign_keys=OFF;
+BEGIN TRANSACTION;
+CREATE TABLE users (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    email TEXT NOT NULL UNIQUE,
+    display_name TEXT NOT NULL,
+    created_at INTEGER NOT NULL
+);
+INSERT INTO users VALUES(1,'admin@asd.com','Admin','10:03:19');
+INSERT INTO users VALUES(2,'bob@asd.com','Bob','10:03:42');
+INSERT INTO users VALUES(3,'sandra@asd.com','Sandra','10:04:48');
+COMMIT;
--- src-tauri/src/schema.rs
+++ src-tauri/src/schema.rs
@@ -1,1 +1,10 @@
 // @generated automatically by Diesel CLI.
+
+diesel::table! {
+    users (id) {
+        id -> Nullable<Integer>,
+        email -> Text,
+        display_name -> Text,
+        created_at -> Integer,
+    }
+}
