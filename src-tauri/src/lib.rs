1fbb0fbf661f8464671ba1ea5e638f000d86a3ba
Nabeel <nabeel@nabeel-macbook.local>
2026-03-29 14:44:35 +0700

    create user table and migrations

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,3 +1,10 @@
+use diesel::prelude::*;
+use dotenv::dotenv;
+use std::env;
+
+pub mod models;
+pub mod schema;
+
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
 #[tauri::command]
 fn greet(name: &str) -> String {
@@ -11,4 +18,11 @@ pub fn run() {
         .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
+}
+
+
+pub fn establish_connection() -> SqliteConnection {
+    dotenv().ok();
+    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+    SqliteConnection::establish(&database_url).expect("Failed to connect to database")
 }
---
+++ src-tauri/src/models.rs
@@ -0,0 +1,11 @@
+use diesel::prelude::*;
+
+#[derive(Queryable, Selectable)]
+#[diesel(table_name = crate::schema::users)]
+#[diesel(check_for_backend(diesel::sqlite::Sqlite))]
+pub struct User {
+    pub id: i32,
+    pub email: String,
+    pub display_name: String,
+    pub created_at: i32,
+}
