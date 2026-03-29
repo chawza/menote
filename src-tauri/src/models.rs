b9b89fcbf853a556ea94a7cb9bade7773d158581
Nabeel KM <nabeelkahlil403@gmail.com>
2026-03-29 17:18:47 +0700

    make id optional

--- src-tauri/src/models.rs
+++ src-tauri/src/models.rs
@@ -4,7 +4,7 @@ use diesel::prelude::*;
 #[diesel(table_name = crate::schema::users)]
 #[diesel(check_for_backend(diesel::sqlite::Sqlite))]
 pub struct User {
-    pub id: i32,
+    pub id: Option<i32>,
     pub email: String,
     pub display_name: String,
     pub created_at: i32,
