17aca09351ff186f73c8477f9b1ce03a200081ac
Nabeel KM <nabeelkahlil403@gmail.com>
2026-04-03 15:26:32 +0700

    rm unsued

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -48,13 +48,6 @@
   .user-table--head {
     text-align: left;
   }
-  .logo.vite:hover {
-    filter: drop-shadow(0 0 2em #747bff);
-  }
-
-  .logo.svelte-kit:hover {
-    filter: drop-shadow(0 0 2em #ff3e00);
-  }
 
   :root {
     font-family: Inter, Avenir, Helvetica, Arial, sans-serif;
@@ -83,42 +76,6 @@
   h1 {
     text-align: center;
   }
-
-  input,
-  button {
-    border-radius: 8px;
-    border: 1px solid transparent;
-    padding: 0.6em 1.2em;
-    font-size: 1em;
-    font-weight: 500;
-    font-family: inherit;
-    color: #0f0f0f;
-    background-color: #ffffff;
-    transition: border-color 0.25s;
-    box-shadow: 0 2px 2px rgba(0, 0, 0, 0.2);
-  }
-
-  button {
-    cursor: pointer;
-  }
-
-  button:hover {
-    border-color: #396cd8;
-  }
-  button:active {
-    border-color: #396cd8;
-    background-color: #e8e8e8;
-  }
-
-  input,
-  button {
-    outline: none;
-  }
-
-  #greet-input {
-    margin-right: 5px;
-  }
-
   @media (prefers-color-scheme: dark) {
     :root {
       color: #f6f6f6;
@@ -127,15 +84,6 @@
 
     a:hover {
       color: #24c8db;
-    }
-
-    input,
-    button {
-      color: #ffffff;
-      background-color: #0f0f0f98;
-    }
-    button:active {
-      background-color: #0f0f0f69;
     }
   }
 </style>
